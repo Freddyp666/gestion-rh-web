827b74e609160236fe9a8606392bab5f573d6f61
Freddyp666 <pat-.-freddy@outlook.com>
2026-02-27 21:20:27 -0500

    Configuracion de app config, agregacions de servicio para obtener lista de empleados y la confijuracion de ruta de la api.

--- src/app/core/service/empleado.service.ts
+++ src/app/core/service/empleado.service.ts
@@ -2,18 +2,19 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
+import { Empleado } from '../models/empleado.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class Empleado {
+export class EmpleadoService {
 
   private http=inject(HttpClient);
 
   private apiUrl = `${environment.apiUrl}/empleados`;
 
   getEmpleados():Observable<Empleado[]> {
-    return this.http.get<Empleado[]>(this.apiUrl);
+    return this.http.get<Empleado[]>(`${this.apiUrl}`);
   }
 
   
--- src/app/features/empleados/lista-empleados/lista-empleados.ts
+++ src/app/features/empleados/lista-empleados/lista-empleados.ts
@@ -1,18 +1,36 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { EmpleadoService } from '../../../core/service/empleado.service';
+import { Empleado } from '../../../core/models/empleado.model';
 
 @Component({
   selector: 'app-lista-empleados',
   imports: [],
-  templateUrl: './lista-empleados.html'
+  templateUrl: './lista-empleados.html',
 })
-export class ListaEmpleados { 
-
-  empleados = [
-    { nombre: "Juan", apellido: "Pérez", email: "juan.perez@empresa.com" },
-    { nombre: "María", apellido: "Gómez", email: "maria.gomez@empresa.com" },
-    { nombre: "Carlos", apellido: "Rodríguez", email: "carlos.rodriguez@empresa.com" }
-  ]
+export class ListaEmpleados {
+  empleados: Empleado[] = [];
+  cargando: boolean = true;  // ← para mostrar loading
+  error: string = '';  // ← para mostrar errores
 
 
+  ngOnInit(){
+    this.obtenerEmpleados();
+  }
 
+  private empleadoService = inject(EmpleadoService);
+
+  private obtenerEmpleados() {
+    this.cargando = true;
+    this.empleadoService.getEmpleados().subscribe({
+      next: (dato) => {
+        this.empleados = dato;
+        this.cargando = false;
+      },
+      error:(error)=>{
+        console.error('Error al obtener empleados:', error);
+        this.error = 'No se pudieron cargar los empleados. Intente nuevamente más tarde.';
+        this.cargando = false;
+      }
+    });
+  }
 }
