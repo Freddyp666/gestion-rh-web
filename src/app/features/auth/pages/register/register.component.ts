e545aa605401cf4eab162c39bfc088b6ca1b5772
Freddyp666 <fredypallo.dev@outlook.com>
2026-05-04 23:59:24 -0500

    Feat: intefaza del login

---
+++ src/app/features/auth/pages/register/register.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'register-component',
+  imports: [],
+  templateUrl: './register.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class RegisterComponent { }
